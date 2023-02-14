474e8c168f3a96185449ca3b3ce5c7373f413f99
Sroda Mensah <mensahsrora36@gmail.com>
2023-02-14 08:25:16 +0000

    banner

--- components/article/index.js
+++ components/article/index.js
@@ -28,7 +28,7 @@ const styles = {
     bannerImage: `object-cover h-auto w-full flex flex-1 `,  
     title: `font-bold text-3xl font-mediumSerif text-[#290916] font-mediumSerif`,
     subtitle: `font-mediumSerifItalic text-[1.4rem] text-[#292929] font-mediumSerif`,
-    article: `font-mediumSerif text-[#292929] text-[1.3rem]`,
+    article: `font-mediumSerif text-[#292929] text-[1.3rem]`
 }
 
 export default function Article({ data, origin }) {
